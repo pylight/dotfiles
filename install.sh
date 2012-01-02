a0f79512c0170172202a2802e47e62839a26fdfb
pylight <intelligenzfluechtling@gmail.com>
2012-01-03 00:38:11 +0100

    install script changed

--- install.sh
+++ install.sh
@@ -7,10 +7,45 @@ createSym()
    tool=$1
    file=$2
 
-   echo "Creating symlink for $tool at ~/.$file"
+   echo "=> Creating symlink for $tool at ~/.$file"
    ln -s $curDir/$file $HOME.$file
+   
+   if [ $? -eq 0 ]; then
+      echo $HOME.$file >> .createdLinks   # remember linked files
+   fi
 }
 
+deleteSyms()
+{
+   lines=0
+   actions=0
+
+   cat .createdLinks | while read LINE
+   do
+      echo "=> Removing $LINE"
+      rm $LINE
+      if [ $? -eq 0 ]; then   # rm successfull?
+         let actions++
+      fi
+      let line++
+   done
+
+   # rm infofile if all 
+   # symlinks are removed
+   if [ "$lines" -eq "$actions" ]
+   then
+      rm .createdLinks
+      echo ""
+      echo "Done. To remove the whole folder, run:"
+      echo "cd ; rm -R $curDir"
+      echo ""
+   else
+      echo ""
+      echo "Error while trying to delete symlinks!"
+      echo "Please check your home directory and the .createdLinks-File manually."
+      echo ""
+   fi
+}
 
 echo "== dotfiles install script =="
 
@@ -20,6 +55,11 @@ then
    createSym tmux tmux.conf
    createSym conky conkyrc
    createSym urxvt Xdefaults
+
+elif [ "$1" == "-uninstall" ]
+then
+   echo "Removing symlinks..."
+   deleteSyms
 else
    echo "Please read the README.md before you run this!"
    echo "Run ./install.sh -doinstall to create symlinks"

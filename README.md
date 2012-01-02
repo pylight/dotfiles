3ad33afcfeb7df44f63ee7698651c8fdef093ee1
pylight <intelligenzfluechtling@gmail.com>
2012-01-02 23:30:34 +0100

    added cokyrc, tmux.conf and install.sh script

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 dotfiles 
 ========
 This repository contains configfiles for my Archlinux systems. Feel free to use or change them to suit your needs.
+
+Installation
+-------------
+
+   git clone https://github.com/pylight/dotfiles.git ~/.dotfiles
+   cd ~/.dotfiles
+   chmod +x install.sh
+
+
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+curDir=$(/bin/pwd)
+
+createSym()
+{
+   tool=$1
+   file=$2
+
+   echo "Creating symlink for $tool at ~/.$file"
+   ln -s $curDir/$file $HOME.$file
+}
+
+
+echo "== dotfiles install script =="
+
+if [ "$1" == "-doinstall" ]
+then
+   # create symlinks
+   createSym tmux tmux.conf
+   createSym conky conkyrc
+else
+   echo "Please read the README.md before you run this!"
+   echo "Run ./install.sh -doinstall to create symlinks"
+fi
+
+
